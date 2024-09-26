9ddb062b6788b61114e05e480aa05a586d4adea6
Ki Song <pobrejuanito@gmail.com>
2024-09-26 15:01:21 -0400

    Update

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
    url(r'^$', views.index, name='home'),
     url(r'^captcha/', include('captcha.urls')),
+    url(r'^privacy-policy.html', TemplateView.as_view(template_name="privacy-policy.html")),
     url(r'^sendmessage/', views.sendmessage, name='sendmessage'),
-    url(r'^member-booking-form/', views.bookingform, name='bookingform'),
+    url(r'^member-booking-form/', views.bookingform, name='bookingform'),11
 ]
